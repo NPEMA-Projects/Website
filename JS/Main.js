a89a570f0ed4112018611a555ede6b4914d6aa93
Ben-Ardiel <45043426+Ben-Ardiel@users.noreply.github.com>
2021-01-13 22:51:12 -0500

    Bruh

---
+++ JS/Main.js
@@ -0,0 +1,14 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+/*
+Above is modal code
+
+Below is photo grid code
+*/
